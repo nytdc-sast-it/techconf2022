d278ebeab64bc13fb77743b4dd12dc4e7a005294
Taoning Ge <gtn1024o@outlook.com>
2022-11-05 15:50:58 +0800

    删除没用的东西

--- submit-fe/src/components/footer/index.vue
+++ submit-fe/src/components/footer/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <a-layout-footer class="footer">Arco Pro</a-layout-footer>
+  <a-layout-footer class="footer">&copy; 2013-2022 NYTDC-SAST</a-layout-footer>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="less" scoped>
-  .footer {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-    color: var(--color-text-2);
-    text-align: center;
-  }
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  color: var(--color-text-2);
+  text-align: center;
+}
 </style>

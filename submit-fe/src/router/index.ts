4ec06ee4f92db83757d20dcdab720f32be9a38b1
Taoning Ge <gtn1024o@outlook.com>
2022-11-05 15:45:39 +0800

    remove useless pages and guard

--- submit-fe/src/router/index.ts
+++ submit-fe/src/router/index.ts
@@ -1,37 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import NProgress from 'nprogress'; // progress bar
 import 'nprogress/nprogress.css';
 
-import { appRoutes } from './routes';
-import { REDIRECT_MAIN, NOT_FOUND_ROUTE } from './routes/base';
-import createRouteGuard from './guard';
-
-NProgress.configure({ showSpinner: false }); // NProgress Configuration
+NProgress.configure({showSpinner: false}); // NProgress Configuration
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      redirect: 'login',
+      name: 'submit',
+      component: () => import('@/views/submit/index.vue'),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/login/index.vue'),
-      meta: {
-        requiresAuth: false,
-      },
-    },
-    ...appRoutes,
-    REDIRECT_MAIN,
-    NOT_FOUND_ROUTE,
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('@/views/submit/index.vue'),
+    }
   ],
   scrollBehavior() {
-    return { top: 0 };
+    return {top: 0};
   },
 });
-
-createRouteGuard(router);
 
 export default router;

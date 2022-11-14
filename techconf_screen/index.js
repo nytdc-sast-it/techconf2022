9bda373bc39d9493780434f4c87ffaa694f7f0f7
Taoning Ge <gtn1024o@outlook.com>
2022-11-14 16:52:28 +0800

    全屏

--- techconf_screen/index.js
+++ techconf_screen/index.js
@@ -1,54 +1,53 @@
 (function () {
-    class Barrage {
-        constructor(id) {
-            this.domList = [];
-            this.dom = document.querySelector('#' + id);
-            if (this.dom.style.position == '' || this.dom.style.position == 'static') {
-                this.dom.style.position = 'relative';
-            }
-            this.dom.style.overflow = 'hidden';
-            let rect = this.dom.getBoundingClientRect();
-            this.domWidth = rect.right - rect.left;
-            this.domHeight = rect.bottom - rect.top;
-        }
-
-        shoot(text) {
-            let div = document.createElement('div');
-            div.style.position = 'absolute';
-            div.style.left = this.domWidth + 'px';
-            div.style.top = (this.domHeight - 20) * +Math.random().toFixed(2) + 'px';
-            div.style.whiteSpace = 'nowrap';
-            div.style.color = '#' + Math.floor(Math.random() * 256).toString(10);
-            div.innerText = text;
-            this.dom.appendChild(div);
-
-            let roll = (timer) => {
-                let now = +new Date();
-                roll.last = roll.last || now;
-                roll.timer = roll.timer || timer;
-                let left = div.offsetLeft;
-                let rect = div.getBoundingClientRect();
-                if (left < (rect.left - rect.right)) {
-                    this.dom.removeChild(div);
-                } else {
-                    if (now - roll.last >= roll.timer) {
-                        roll.last = now;
-                        left -= 3;
-                        div.style.left = left + 'px';
-                    }
-                    requestAnimationFrame(roll);
-                }
-            }
-            roll(50 * +Math.random().toFixed(2));
-        }
-
+  class Barrage {
+    constructor(id) {
+      this.dom = document.querySelector(id);
+      if (this.dom.style.position == "" || this.dom.style.position == "static") {
+        this.dom.style.position = "relative";
+      }
+      this.dom.style.overflow = "hidden";
+      let rect = this.dom.getBoundingClientRect();
+      this.domWidth = rect.right - rect.left;
+      this.domHeight = rect.bottom - rect.top;
     }
 
-    const barage = new Barrage('content');
+    shoot(text) {
+      let div = document.createElement("div");
+      div.style.position = "absolute";
+      div.style.left = this.domWidth + "px";
+      div.style.top = (this.domHeight - 20) * +Math.random().toFixed(2) + "px";
+      div.style.whiteSpace = "nowrap";
+      div.style.color = "#" + Math.floor(Math.random() * 256).toString(10);
+      div.style.fontSize = Math.floor(Math.random() * 20 + 10) + "px";
+      div.innerText = text;
+      this.dom.appendChild(div);
 
-    const ws = new WebSocket('wss://techconf.sastit.com/api/ws');
-    ws.onmessage = (e) => {
-       const data = JSON.parse(e.data)
-       barage.shoot(data.danmu)
+      let roll = (timer) => {
+        let now = +new Date();
+        roll.last = roll.last || now;
+        roll.timer = roll.timer || timer;
+        let left = div.offsetLeft;
+        let rect = div.getBoundingClientRect();
+        if (left < rect.left - rect.right) {
+          this.dom.removeChild(div);
+        } else {
+          if (now - roll.last >= roll.timer) {
+            roll.last = now;
+            left -= 3;
+            div.style.left = left + "px";
+          }
+          requestAnimationFrame(roll);
+        }
+      };
+      roll(50 * +Math.random().toFixed(2));
     }
+  }
+
+  const barage = new Barrage("body");
+
+  const ws = new WebSocket("wss://techconf.sastit.com/api/ws");
+  ws.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    barage.shoot(data.danmu);
+  };
 })();
